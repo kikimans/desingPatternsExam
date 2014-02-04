c65c26402618013450b6953a0787f9990347d384
kikimans <kikimans@gmail.com>
2014-02-05 02:27:43 +0900

    chatper 1 exam complete

--- src/test/java/com/kikimans/designPatternsExam/chapter1/TestQuackBehavior.java
+++ src/test/java/com/kikimans/designPatternsExam/chapter1/TestQuackBehavior.java
@@ -1,7 +1,5 @@
 package com.kikimans.designPatternsExam.chapter1;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import com.kikimans.designPatternsExam.chapter1.impl.QuackBehavior;
@@ -13,6 +11,18 @@ public class TestQuackBehavior {
 	@Test
 	public void test_꽥꽥거리다() {
 		behavior = new Quack();
+		behavior.quack();
+	}
+	
+	@Test
+	public void test_삑삑거리다(){
+		behavior = new Squeak();
+		behavior.quack();
+	}
+	
+	@Test 
+	public void test_소리가나지않는다(){
+		behavior = new MuteQuack();
 		behavior.quack();
 	}
 
